53d128e7fe2b57253f274e5e4f5e211a3620a831
nicholas <nicholasovunda@gmail.com>
2022-11-11 15:28:35 +0100

    step-3 add products, maker and product catgeory to product app

---
+++ product/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(ProductCategory)
+admin.site.register(Maker)
+admin.site.register(Product)
+
+# Register your models here.
